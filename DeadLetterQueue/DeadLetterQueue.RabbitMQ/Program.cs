177440d3ac2ed4f389f1e610e3e80b717913dc73
Разнополов Андрей Сергеевич <andrey.raznopolov@ligastavok.ru>
2023-04-22 21:03:25 +0300

    rmq hello world

---
+++ DeadLetterQueue/DeadLetterQueue.RabbitMQ/Program.cs
@@ -0,0 +1,93 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("Hello, World!");
+
+var cts = new CancellationTokenSource();
+var host = "localhost";
+
+try
+{
+    Task.Run(() => ProducerLoop(host, cts.Token));
+    Task.Run(() => ConsumerLoop(host, cts.Token));
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+cts.Cancel();
+
+async Task ProducerLoop(string host, CancellationToken cancellationToken)
+{
+    var factory = new ConnectionFactory()
+    {
+        HostName = host,
+        Port = 5672,
+        UserName = "rmuser",
+        Password = "rmpassword",
+    };
+    
+    Console.WriteLine("Starting producer");
+    using var connection = factory.CreateConnection();
+
+    using var channel = connection.CreateModel();
+
+    channel.QueueDeclare("test-queue", false, false, false);
+    int count = 0;
+    
+    var body = "iteration"u8.ToArray();
+
+    Console.WriteLine("Enable sending loop");
+    while (!cancellationToken.IsCancellationRequested)
+    {
+        await Task.Delay(3000);
+        
+        channel.BasicPublish(exchange: string.Empty,
+            routingKey: "test-queue",
+            basicProperties: null,
+            body: body);
+        Console.WriteLine($" [x] message sent");
+    }
+}
+
+async Task ConsumerLoop(string host, CancellationToken cancellationToken)
+{
+    var factory = new ConnectionFactory()
+    {
+        HostName = host,
+        Port = 5672,
+        UserName = "rmuser",
+        Password = "rmpassword",
+    };
+    
+    Console.WriteLine("Starting consumer");
+    using var connection = factory.CreateConnection();
+
+    using var channel = connection.CreateModel();
+    
+    channel.QueueDeclare("test-queue", false, false, false);
+
+    var consumer = new EventingBasicConsumer(channel);
+
+    consumer.Received += (sender, eventArgs) =>
+    {
+        var body = eventArgs.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+
+        Console.WriteLine($"Consumed: {message}");
+    };
+
+    channel.BasicConsume("test-queue", true, consumer);
+
+    while (!cancellationToken.IsCancellationRequested)
+    {
+        await Task.Delay(1000);
+    }
+}
